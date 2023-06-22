f325ceec0d8f7e23e915a3061d64d370c19631d3
fabiopamaral <fabio.pamaral@outlook.com>
2023-06-22 20:42:14 -0300

    Commit Inicial do Projeto

---
+++ src/main/java/com/gerenciador/tarefas/rest/api/gerenciadortarefasrestapi/GerenciadorTarefasRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciador.tarefas.rest.api.gerenciadortarefasrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorTarefasRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorTarefasRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciador/tarefas/rest/api/gerenciadortarefasrestapi/GerenciadorTarefasRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.tarefas.rest.api.gerenciadortarefasrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorTarefasRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
